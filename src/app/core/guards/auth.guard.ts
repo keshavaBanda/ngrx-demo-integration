9eb7bdfd0a518a21b3ecc0af01f6efb369d84500
keshavaBanda <keshava9930@gmail.com>
2026-09-07 11:59:50 +0530

    folder-structure-changes

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,6 +9,5 @@ export const authGuard: CanActivateFn = ()=>{
   if(authService.isAuthenticated()){
     return true;
   }
-  return router.createUrlTree(['/login'])
-
+  return router.createUrlTree(['/login']);
 }
